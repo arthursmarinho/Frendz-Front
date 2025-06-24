a1491319a3983601ba2cbbd9910ee14a72112c36
arthursmarinho <arthursnat@gmail.com>
2025-06-23 22:29:42 -0300

    fix: update viewport meta tag for improved scaling and consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </head>
 
